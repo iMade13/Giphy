5c35df794c45131a80537fedbe9e2563aec96ea8
Madeleine Marcano <madeleine1302@gmail.com>
2018-06-18 18:38:04 -0400

    ultimo de hoy 18-16

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,19 @@
-fetch(`http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=rRW01ltrDi8VlAZuV1pilP1bcPQExnUB`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        renderInfo(data);
-    })
+const inputText = document.querySelector("input")
+
+inputText.addEventListener("keypress", () => {
+            let dibujarGifs = data => {
+                let gif = '';
+                let url = '';
+                data.forEach(element => {
+                    gif =
+
+                });
+            }
+        }
+
+        fetch(`http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=rRW01ltrDi8VlAZuV1pilP1bcPQExnUB`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            renderInfo(data);
+        })

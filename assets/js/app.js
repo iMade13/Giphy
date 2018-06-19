c567784630e182fce4db5e77612a72abc0f6733d
Madeleine Marcano <madeleine1302@gmail.com>
2018-06-18 22:59:29 -0400

    corriendo la data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,23 @@
 const inputText = document.querySelector("input")
+const buscarGif = document.querySelector("button")
+const containerImage = document.getElementById("elemento")
 
-inputText.addEventListener("keypress", () => {
-            let dibujarGifs = data => {
-                let gif = '';
-                let url = '';
-                data.forEach(element => {
-                    gif =
+buscarGif.addEventListener("click", () => {
+    inputText.value = "";
 
-                });
-            }
-        }
-
-        fetch(`http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=rRW01ltrDi8VlAZuV1pilP1bcPQExnUB`)
+    fetch(`http://api.giphy.com/v1/gifs/search?q=${gif}&api_key=rRW01ltrDi8VlAZuV1pilP1bcPQExnUB`)
         .then(response => response.json())
         .then(data => {
             console.log(data);
-            renderInfo(data);
         })
+})
+
+const renderInfo = data => {
+    let gif = '';
+    let url = '';
+    data.forEach(element => {
+        gif = element.images.downsized_large.url;
+        url = element.bitly_gif_url;
+        containerImage.innerHTML = `<img src="${data.gif}'>`;
+    });
+}
